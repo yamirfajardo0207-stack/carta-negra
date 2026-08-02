fa3e04b8b0edee724b1556814a1842c37694778a
yamirfajardo0207-stack <yamirfajardo0207@gmail.com>
2026-08-01 19:57:26 -0500

    Add files via upload

---
+++ carta2.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(page_title="Carta especial", page_icon="ฅ(^◉ㅅ◉^ )ฅ")
+
+st.title("Carta para una negra")
+
+if st.button("no pulses aqui"):
+    st.write("---")
+    st.header("TU puedes bro")
+    st.write("""
+ Yo más que nadie sé lo mucho que te esfuerzas, bro. Eres la persona más inteligente que conozco y sé que puedes lograr todo lo que te propongas. Te hice esto por 2 razones:
+
+-Para presumir que estoy aprendiendo a programar.
+-Para que sepas que siempre va a haber alguien apoyándote sin importar lo que pase.
+
+Tú eres muy capaz de todo y espero que te vaya muy bien en la prueba de la UP y puedas cursar la carrera de Derecho (así de paso me mantienes).
+Se le aprecia mucho.
+Ya mucha cursilería 🖕
+    """)
+    st.snow()
